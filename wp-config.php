ad9d3e27a5a4be4ad9b886a8a3d8cff4894905a2
caleb <calebizqui@gmail.com>
2025-09-09 19:24:27 -0500

    update sql

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress_prueba' );
+define( 'DB_NAME', 'wordpress_test_clone' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
